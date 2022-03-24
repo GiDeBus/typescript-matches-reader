4eec9958fd5a7addc8b0f9b44c4a62ecc2d9bf2d
Gil <gidelbusto@gmail.com>
2022-03-24 20:37:21 -0300

    read csv file logic

--- build/index.js
+++ build/index.js
@@ -1,1 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', { encoding: 'utf-8' }).split('\n').map((row) => {
+    return row.split(',');
+});
+var MatchResults;
+(function (MatchResults) {
+    MatchResults["HomeWin"] = "H";
+    MatchResults["AwayWin"] = "A";
+    MatchResults["Draw"] = "D";
+})(MatchResults || (MatchResults = {}));
+let manchesterUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manchesterUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manchesterUnitedWins++;
+    }
+}
+console.log(manchesterUnitedWins);
