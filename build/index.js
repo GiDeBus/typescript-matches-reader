7f7176b273e54539627f49cbe20b4ec10558ebae
Gil <gidelbusto@gmail.com>
2022-03-31 19:10:39 -0400

    js compiler

--- build/index.js
+++ build/index.js
@@ -1,20 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const CsvClassReader_1 = require("./CsvClassReader");
+const MatchResults_1 = require("./MatchResults");
 const reader = new CsvClassReader_1.CsvClassReader('football.csv');
 reader.read();
-var MatchResults;
-(function (MatchResults) {
-    MatchResults["HomeWin"] = "H";
-    MatchResults["AwayWin"] = "A";
-    MatchResults["Draw"] = "D";
-})(MatchResults || (MatchResults = {}));
 let manchesterUnitedWins = 0;
 for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+    if (match[1] === 'Man United' && match[5] === MatchResults_1.MatchResults.HomeWin) {
         manchesterUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+    else if (match[2] === 'Man United' && match[5] === MatchResults_1.MatchResults.AwayWin) {
         manchesterUnitedWins++;
     }
 }
--- build/utils.js
+++ build/utils.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dateStringToDate = void 0;
 const dateStringToDate = (dateString) => {
-    const dateParts = dateString.split('/').map((value) => {
-        parseInt(value);
+    const dateParts = dateString
+        .split('/')
+        .map((value) => {
+        return parseInt(value);
     });
     return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
 };
