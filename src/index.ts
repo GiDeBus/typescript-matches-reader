e2318045b6ccaa98eb93e00b69beec29bd92d3d5
Gil <gidelbusto@gmail.com>
2022-04-01 19:09:28 -0400

    composition over inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import { CsvClassReader } from "./CsvClassReader";
+import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResults } from './MatchResults';
 
-const reader = new CsvClassReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manchesterUnitedWins = 0;
 
-for (let match of reader.data) { 
+for (let match of matchReader.matches) { 
   if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
     manchesterUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) { 
